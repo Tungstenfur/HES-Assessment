9bc1839caec7e6690648a251618e6074f92a0a89
tungsten <koszutowski.bartek@gmail.com>
2026-08-21 10:10:56 +0200

    Part 2

--- Part-2/script.js
+++ Part-2/script.js
@@ -1,3 +1,46 @@
+async function DownloadLogs() {
+  const log_div = document.getElementById("logs");
+  try {
+    const response = await fetch("latest.log");
+
+    if (!response.ok) {
+      log_div.innerHTML = "<p class='error'>Error fetching logs.</p>";
+      return;
+    }
+    const logs = await response.text();
+    const lines = logs.split("\n");
+    const logFragment = document.createDocumentFragment();
+
+    for (const line of lines) {
+      const match = line.match(/\[[^/]+\/([^\]]+)\]/);
+      let type = (match ? match[1] : "INFO").toLowerCase();
+      if (
+        type !== "info" &&
+        type !== "error" &&
+        type !== "fatal" &&
+        type !== "maintenance" &&
+        type !== "warn"
+      ) {
+        type = "info";
+      }
+      const logLine = document.createElement("p");
+      logLine.className = type;
+      logLine.textContent = line;
+      logFragment.append(logLine);
+    }
+
+    log_div.replaceChildren(logFragment);
+  } catch (error) {
+    log_div.innerHTML =
+      "<p class='error'>Error fetching logs. If youre running this through opening html file through chromium engine based browser it might be blocking the request due to CORS.</p>" +
+      "<p>" +
+      error +
+      "</p>";
+
+    return;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const generateRandomIntArray = (length, min, max) =>
     Array.from(
@@ -5,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
       () => Math.floor(Math.random() * (max - min + 1)) + min,
     );
 
-  const numberOfHours = 20;
+  const numberOfHours = 24;
   const currentFullHour = new Date();
   currentFullHour.setMinutes(0, 0, 0);
   const hourLabels = Array.from({ length: numberOfHours }, (_, index) => {
@@ -29,10 +72,9 @@ document.addEventListener("DOMContentLoaded", function () {
       datasets: [
         {
           label: "Ping Time (ms)",
-          data: generateRandomIntArray(numberOfHours, 90, 150),
+          data: generateRandomIntArray(numberOfHours, 80, 150),
           borderColor: "rgba(75, 192, 192, 1)",
           fill: false,
-          tension: 0.1,
         },
       ],
     },
@@ -56,4 +98,5 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
+  DownloadLogs();
 });
